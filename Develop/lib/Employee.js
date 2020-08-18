c941aed1851ec2c2d69fe7d6e1bb5d9b0f0bb034
Frank Buono <fbuonojr@fordham.edu>
2020-08-18 16:38:37 -0400

    fixed constructor

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,17 +1,20 @@
 // TODO: Write code to define and export the Employee class
 class Employee {
     constructor(name, id, email) {
-        if (!name) {
-            throw new Error("You are missing name");
-        }
+        // if (!name) {
+        //     throw new Error("You are missing name");
+        // }
 
-        if (!id) {
-            throw new Error("You are missing id");
-        }
+        // if (!id) {
+        //     throw new Error("You are missing id");
+        // }
 
-        if (!email) {
-            throw new Error("You are missing email");
-        }
+        // if (!email) {
+        //     throw new Error("You are missing email");
+        // }
+        this.name = name;
+        this.id = id;
+        this.email = email;
     }
 
     getName() {
